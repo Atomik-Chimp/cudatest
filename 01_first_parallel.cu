36e3efa4afbd3ea6612210e7a0379f2e33e658c9
Justin Breese <particle_m@hotmail.com>
2021-10-27 21:02:00 -0800

    Update 01_first_parallel.cu
    Fixed the code, copied it over from hello_gpu where it was mistakenly placed.

--- 01_first_parallel.cu
+++ 01_first_parallel.cu
@@ -2,9 +2,10 @@
 
 /*
  * Refactor firstParallel so that it can run on the GPU.
+ * << added __global__ in front of the function >>
  */
 
-void firstParallel()
+__global__ void firstParallel()
 {
   printf("This should be running in parallel.\n");
 }
@@ -12,15 +13,37 @@ void firstParallel()
 int main()
 {
   /*
-   * Refactor this call to firstParallel to execute in parallel
+   * Refactor this call to firstParallel to execute in parallel once
    * on the GPU.
    */
 
-  firstParallel();
+  firstParallel<<<1,1>>>();
+  cudaDeviceSynchronize();  // is needed or all the white space finishes first
+  
+  printf("\n"); // add a blank space
+  
+  /*
+   * Refactor this call to firstParallel to execute in parallel in 5 threads
+   * on the GPU.
+   */
+   
+  firstParallel<<<1,5>>>();
+  cudaDeviceSynchronize();  // is needed or all the white space finishes first
 
+  printf("\n"); // add a blank space
+  
+  /*
+   * Refactor this call to firstParallel to execute in parallel in 5 blocks
+   * of 5 threads on the GPU.
+   */
+  
+  firstParallel<<<5,5>>>();
+  
   /*
    * Some code is needed below so that the CPU will wait
    * for the GPU kernels to complete before proceeding.
    */
+   
+   cudaDeviceSynchronize();
 
 }
