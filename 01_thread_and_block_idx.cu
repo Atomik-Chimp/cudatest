9de52482bf571a99a9358c76c15a648c6b2a5f86
Justin Breese <particle_m@hotmail.com>
2021-11-01 10:18:40 -0800

    Update 01_thread_and_block_idx.cu
    Added the code to make this work two ways...

--- 01_thread_and_block_idx.cu
+++ 01_thread_and_block_idx.cu
@@ -7,7 +7,20 @@ __global__ void printSuccessForCorrectExecutionConfiguration()
   {
     printf("Success!\n");
   } else {
-    printf("Failure. Update the execution configuration as necessary.\n");
+    //printf("Failure. Update the execution configuration as necessary.\n");
+    // commented out to prevent failure spam
+  }
+}
+
+__global__ void printSuccessForCorrectExecutionConfiguration2()
+{
+
+  if(threadIdx.x == 1 && blockIdx.x == 1)
+  {
+    printf("Success!\n");
+  } else {
+    //printf("Failure. Update the execution configuration as necessary.\n");
+    // commented out to prevent failure spam
   }
 }
 
@@ -16,7 +29,17 @@ int main()
   /*
    * Update the execution configuration so that the kernel
    * will print `"Success!"`.
+   * Updated to run enough Kernels
    */
 
-  printSuccessForCorrectExecutionConfiguration<<<1, 1>>>();
+  printSuccessForCorrectExecutionConfiguration<<<256, 1024>>>();
+  cudaDeviceSynchronize(); //remember this to let the GPU catch up before the program exits
+
+  /*
+   * Do it a second way...
+   *
+   */
+  
+  printSuccessForCorrectExecutionConfiguration2<<<2, 2>>>();
+  cudaDeviceSynchronize(); //remember this to let the GPU catch up before the program exits 
 }
